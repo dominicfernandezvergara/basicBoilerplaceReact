89721b04ae88f23358c56dde64bfecb7b30ce65a
dominicfernandezvergara <dominic.fernandezv@gmail.com>
2021-03-26 12:41:14 -0300

    deleted all pages

--- src/App.js
+++ src/App.js
@@ -2,30 +2,16 @@ import React, { Fragment } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 import './App.css';
-import Header from './components/header';
 
 import Home from './pages/home';
-import Services from './pages/services';
-import Contact from './pages/contact-us';
-import About from './pages/about';
 
 function App() {
   return (
     <Fragment>
       <Router>
-        <Header />
         <Switch>
           <Route path="/inicio">
             <Home />
-          </Route>
-          <Route path="/quien-soy">
-            <About />
-          </Route>
-          <Route path="/servicios">
-            <Services />
-          </Route>
-          <Route path="/contacto">
-            <Contact />
           </Route>
           <Route path="*">
             <Home />
--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,26 +1,11 @@
 import React from 'react';
 
 import styles from './home.module.css';
-import Banner from '../../components/home-components/banner';
-import ProblemSectionHome from '../../components/home-components/problem-section-home';
-import EmpathySectionHome from '../../components/home-components/empathy-section-home';
-import FourthSectionHome from '../../components/home-components/services-section-home';
-import Footer from '../../components/footer';
 import UseScrollToTop from '../../hooks/use-scroll-to-top';
-import HowItWork from '../../components/home-components/how-it-work';
 
 function Home() {
   UseScrollToTop();
-  return (
-    <div className={styles.containerHome}>
-      <Banner />
-      <ProblemSectionHome />
-      <EmpathySectionHome />
-      <FourthSectionHome />
-      <HowItWork />
-      <Footer />
-    </div>
-  );
+  return <div className={styles.containerHome}>home page</div>;
 }
 
 export default Home;
